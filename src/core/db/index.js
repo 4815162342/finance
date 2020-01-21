108b683a4b17be521c42fa994f55965316efc9bf
Adam Sanders <adam@hux.com>
2020-01-21 09:31:55 -0500

    var name
    Make it more clear that it's for internal use only

--- src/core/db/index.js
+++ src/core/db/index.js
@@ -2,7 +2,7 @@ import ObjectHash from 'object-hash';
 
 export default class Database {
 	name = "FinanceManager";
-	db = null;
+	_db = null;
 	objectStoreNames = [
 		'Transactions',
 	];
@@ -11,38 +11,38 @@ export default class Database {
 		const dbRequest = indexedDB.open(this.name);
 		dbRequest.onerror = console.log;
 		dbRequest.onsuccess = () => {
-			this.db = dbRequest.result;
+			this._db = dbRequest.result;
 			
 			this.objectStoreNames.forEach(ob => {
 				this[ob] = {
 					get: (input, callback) => {
 						if (input instanceof Array)
 							input = IDBKeyRange.bound(input[0], input[1])
-						const getRequest = this.db.transaction(ob, "readwrite").objectStore(ob).get(input);
+						const getRequest = this._db.transaction(ob, "readwrite").objectStore(ob).get(input);
 						
 						getRequest.onsuccess = () => callback(getRequest.result);
 					},
 					put: input => {
 						input._id = ObjectHash(input, {algorithm: 'sha1'});
-						return this.db.transaction(ob, "readwrite").objectStore(ob).put(input);
+						return this._db.transaction(ob, "readwrite").objectStore(ob).put(input);
 					}
 				}
 			});
 		};
 		dbRequest.onupgradeneeded = () => {
-			this.db = dbRequest.result;
+			this._db = dbRequest.result;
 			this.objectStoreNames.forEach(ob => {
-				this.db.createObjectStore(ob, {keyPath: "_id"});
+				this._db.createObjectStore(ob, {keyPath: "_id"});
 			})
 		};
 	};
 	
 	reset() {
 		if (!window.confirm("Are you sure you want to delete all data?")) return;
-		this.objectStoreNames.forEach(ob => this.db.deleteObjectStore(ob));
+		this.objectStoreNames.forEach(ob => this._db.deleteObjectStore(ob));
 	}
 	
 	close() {
-		this.db.close();
+		this._db.close();
 	}
 }
